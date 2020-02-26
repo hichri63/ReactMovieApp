0784dd8fc2be93871a92aa9568a704e2b560ad96
hichri63 <slimhichrri@gmail.com>
2020-02-26 10:22:55 +0100

    ReactMovieApp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Search from './search';
+import Movies from './movies';
+import AddModal from './modal';
+import Rating from './rating';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={ 
+  movies : [{
+  name:'apocalypto',
+  date:'2006',
+  image:"https://fr.web.img5.acsta.net/medias/nmedia/18/36/26/80/18837905.jpg",
+  rating:'4'
+},
+{
+  name:'300',
+  date:'2006',
+  image:"https://i.pinimg.com/originals/6d/db/4d/6ddb4d9e9cfa507ad58f4e5cf26a5c19.jpg",
+  rating:'4'
+},
+{
+  name:'Avatar',
+  date:'2009',
+  image:'https://gold-n-blog.fr/wp-content/uploads/2018/01/avatar_2_poster.jpg',
+  rating:'3',
+
+},
+],
+keyWord:"",
+star:1
+}
+ 
+
+
+
+
+
+
+}
+searchMovies=keyWord=>{
+  this.setState({keyWord:keyWord})
+}
+searchStar=x=>{
+  this.setState({star:x})
+}
+
+
+add = movie => {
+  this.setState({movies : this.state.movies.concat(movie)})
+}
+render() {
+  return (<div>
+    <Search searchMovies={x=>this.searchMovies(x)} />
+    <AddModal add={x => this.add(x)}/> 
+    <Movies movies={this.state.movies.filter(el=>el.rating>=this.state.star&&el.name.toLowerCase().includes(this.state.keyWord.toLowerCase().trim()))}/>
+    <Rating rating={star => this.searchStar(star)} star={this.state.star} />
+    
     </div>
+
+
   );
+}
+
 }
 
 export default App;
